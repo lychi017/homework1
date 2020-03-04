00a2ffce94241fa1f330cafef1892b857cccccec
RyanLin <yt5042@euntay.tw>
2020-03-04 19:32:28 +0800

    Initial Commit

---
+++ homework1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework1
+//
+//  Created by 林裕棋 on 2020/3/4.
+//  Copyright © 2020 RyanLin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
